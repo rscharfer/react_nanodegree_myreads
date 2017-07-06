9e5aa7744c49dc05ab4b12920b051bd65adacd2d
Ryan <rscharfer@gmail.com>
2017-07-06 08:27:20 +0200

    added first shelf component

--- src/App.js
+++ src/App.js
@@ -36,6 +36,24 @@ class BookShelfChanger extends React.Component {
 }
 
 
+class BookShelf extends React.Component {
+
+  render(){
+
+    return <div className="bookshelf">
+            <h2 className="bookshelf-title">{this.props.label}</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {this.props.books.map(book=>{ 
+                    return <li><Book title={book.title} author={book.author} url={book.url}/></li>
+                  })}
+                  </ol>
+              </div>
+            </div>
+  }
+}
+
+
 
 
 
@@ -78,19 +96,7 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <Book title="To Kill a Mockingbird" author="Harper Lee" url="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"/>
-                      </li>
-                      <li>
-                        <Book title="Ender's Game" author="Orson Scott Card" url="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"/>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
+                <BookShelf label="Currently Reading" books={[{title:"To Kill a Mockingbird",author:"Harper Lee",url:"http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"},{title:"Ender's Game",author:"Orson Scott Card",url:"http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"}]}/>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
