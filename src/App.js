81aa5c424d0984ed111709272db89b3147732e1a
Ryan <rscharfer@gmail.com>
2017-07-12 09:55:57 +0200

    initial content taken from the server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react'
-import { Component } from 'react'
-import {Link, Route} from 'react-router-dom'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+import { Component } from 'react';
+import {Link, Route} from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
 
 
 
 class BooksApp extends Component {
  
+  constructor(props){
+
+    super(props);
+    this.state = {wantToReadBooks:[],readBooks:[],currentlyReadingBooks:[]};
+    this.refreshBooks = this.refreshBooks.bind(this);
+    
+
+
+  }
+
+  componentDidMount(){
+
+      this.refreshBooks();
+  }
+
+
+  refreshBooks(){
+
+    BooksAPI.getAll().then(books=>{
+
+      this.setState({
+        wantToReadBooks : books.filter(book=>book.shelf==="wantToRead"),
+        readBooks : books.filter(book=>book.shelf==="read"),
+        currentlyReadingBooks : books.filter(book=>book.shelf==="currentlyReading")
+      });
+      console.log(this.state.wantToReadBooks);
+    });
+  }
+
   render() {
+
     return (
       <div className="app">
 
@@ -19,9 +49,9 @@ class BooksApp extends Component {
             <TitleBar/>
             <div className="list-books-content">
               <div>
-                <BookShelf label="Currently Reading" books={[{title:"To Kill a Mockingbird",author:"Harper Lee",url:"http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"},{title:"Ender's Game",author:"Orson Scott Card",url:"http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"}]}/>
-                <BookShelf label="Want to Read" books={[{title:"1776",author:"David McCullough",url:"http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"},{title:"Harry Potter and the Sorcerer's Stone",author:"J.K. Rowling",url:"http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"}]}/>
-                <BookShelf label="Read" books={[{title:"The Hobbit",author:"J.R.R. Tolkien",url:"http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"},{title:"Oh, the Places You'll Go",author:"Seuss",url:"http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"},{title:'The Adventures of Tom Sawyer',author:'Mark Twain',url:'http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api'}]}/>
+                <BookShelf label="Currently Reading" books={this.state.currentlyReadingBooks}/>
+                <BookShelf label="Want to Read" books={this.state.wantToReadBooks}/>
+                <BookShelf label="Read" books={this.state.readBooks}/>
               </div>
             </div>
             <div className="open-search">
@@ -30,7 +60,7 @@ class BooksApp extends Component {
           </div>
           )}/>
       </div>
-    )
+    );
   }
 }
 
@@ -53,7 +83,7 @@ class BookShelf extends Component {
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   {this.props.books.map((book,index)=>{ 
-                    return <li key={index}><Book label={this.props.label} title={book.title} author={book.author} url={book.url}/></li>
+                    return <li key={index}><Book label={this.props.label} title={book.title} author={book.authors[0]} url={book.imageLinks.thumbnail}/></li>
                   })}
                   </ol>
               </div>
