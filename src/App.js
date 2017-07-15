41a1a0c8ee832fa614af79bceaee12ab90aaa860
Ryan <rscharfer@gmail.com>
2017-07-15 12:27:15 +0200

    moved Book module to own file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import {Link, Route} from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
+import Book from './Book.js'
 
 
 
@@ -93,39 +94,7 @@ class BookShelf extends Component {
   }
 }
 
-class Book extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {shelf:''};
-    this.changeShelf = this.changeShelf.bind(this)
-  }
-
-  changeShelf(newShelf){
-
-    this.setState({shelf:newShelf});
-    BooksAPI.update({id:this.props.id},newShelf).then((data)=>this.props.refreshBooks());
-
-  }
-
-
-  
-
-  render() {
-
-    return  <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.url})`}}></div>
-                    <BookShelfChanger label={this.props.label} changeShelf={this.changeShelf}/>
-                  </div>
-                <div className="book-title">{this.props.title}</div>
-                {
-                  this.props.author?this.props.author.map(author=><div key={author} className="book-authors">{author}</div>):''
-
-                }
-              </div>      
-  }
-}
 
 class BookShelfChanger extends Component {
 
---
+++ src/Book.js
@@ -0,0 +1,36 @@
+class Book extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {shelf:''};
+    this.changeShelf = this.changeShelf.bind(this)
+  }
+
+  changeShelf(newShelf){
+
+    this.setState({shelf:newShelf});
+    BooksAPI.update({id:this.props.id},newShelf).then((data)=>this.props.refreshBooks());
+
+  }
+
+
+  
+
+  render() {
+
+    return  <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.url})`}}></div>
+                    <BookShelfChanger label={this.props.label} changeShelf={this.changeShelf}/>
+                  </div>
+                <div className="book-title">{this.props.title}</div>
+                {
+                  this.props.author?this.props.author.map(author=><div key={author} className="book-authors">{author}</div>):''
+
+                }
+              </div>      
+  }
+}
+
+
+export default Book
