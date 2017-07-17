93455d1f7c93d6c50ab021724cca20998a930bf9
Ryan <rscharfer@gmail.com>
2017-07-17 15:25:24 +0200

    using random key generator to give keys to found books

--- src/App.js
+++ src/App.js
@@ -170,8 +170,20 @@ class SearchResults extends Component {
     return <div className="search-books-results">
               <ol className="books-grid">
               {Array.isArray(this.props.books)&&this.props.books.map((book,index)=>{
-                   
-                    if (book.imageLinks) return <li key={book.id}><Book id={book.id} title={book.title} refreshBooks = {this.props.refreshBooks} author={book.authors} label={book.shelf} url={book.imageLinks.thumbnail}/></li>
+                    function makeid() {
+                      // from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+                      var text = "";
+                      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                      for (var i = 0; i < 5; i++)
+                        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+                          return text;
+                    }
+                    if (book.imageLinks) {
+
+                      return <li key={makeid()}><Book id={book.id} title={book.title} refreshBooks = {this.props.refreshBooks} author={book.authors} label={book.shelf} url={book.imageLinks.thumbnail}/></li>
+                    } 
                   })}</ol>
             </div>
   }
